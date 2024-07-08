6a9331fbdd4963e94ed20ea07fda56c3f41ab024
Rodrigo Molter <rodrigo.molter@gmail.com>
2024-07-07 21:20:13 -0300

    refactor: melhor encapsulamento das funções
    Tratamento de erros;
    Inserção de typehints;
    Nome de variáveis e funções mais concisas

--- login.py
+++ login.py
@@ -1,32 +1,59 @@
 import requests
-import json 
+import json
+import sys
+import usuario as user
 
 ENDPOINT='https://desafiopython.jogajuntoinstituto.org/api/users/login/'
 
-def create_user() -> dict:
-  
-  data = {
+def get_registered_user() -> dict:
+  return user.create_and_regiter_user()
 
-    
-    "email": "maria61@example.com",
-    "password": "9(2KayXpXf"
-  }
-  return data
- 
+def login_by_api(email: str, password: str) -> dict: 
+	"""
+	Realiza login na API com o email e senha fornecidos.
 
-def register_user(data): 
-   response = requests.post(ENDPOINT , json=data)
-   print(response.json())
-   savetofile(response.json())
+	Args:
+			email (str): O email do usuário.
+			password (str): A senha do usuário.
 
-def savetofile(data):
- with open('token.json', 'w') as file:
-    json.dump(data, file, ensure_ascii=True)
+	Returns:
+			dict: A resposta JSON da API se o login for bem-sucedido.
+
+	Exit:
+			O programa com uma mensagem de erro se o login não for bem-sucedido.
+	"""
+	response: requests.Response = requests.post(ENDPOINT, json={"email": email,"password": password})
+	if response.status_code == 200:
+		print("Login realizado com sucesso!")
+		return response.json()
+	else:
+		sys.exit("ERR: Houve um problema ao registrar o usuário")
+
+def save_to_file(data: dict) -> None:
+	"""
+	Salva os dados fornecidos em um arquivo JSON.
+
+	Args:
+			data (dict): Os dados a serem salvos no arquivo.
+
+	Raises:
+		FileNotFoundError: Se o arquivo/PATH a ser salvo não for encontrado
+		IOError: Se houver algum erro durante o salvamento do arquivo
+	"""
+	PATH = "token.json"
+	try: 
+		with open(PATH, 'w') as file:
+			json.dump(data, file, ensure_ascii=True)
+			print(f"Acesso salvo com sucesso em {PATH}")
+	except FileNotFoundError:
+		print(f"Arquivo {PATH} não encontrado...")
+	except IOError:
+		print(f"Não foi possível escrever em {PATH}")
    
+def main() -> None:
+   user = get_registered_user() 
+   token = login_by_api(user['email'], user['password'])
+   save_to_file(token)
 
-def main():
-   data = create_user() 
-   register_user(data)
-
-
-main()
+if __name__ == "__main__":
+   main()
--- usuario.py
+++ usuario.py
@@ -1,13 +1,18 @@
 import requests
+import sys
 from faker import Faker
 
 ENDPOINT='https://desafiopython.jogajuntoinstituto.org/api/users/'
 
+def generate_persona() -> dict:
+	"""
+	Gera uma persona fictícia com dados como nome de usuário, email, senha, telefone, endereço e CPF.
 
-def create_user() -> dict:
-  fake = Faker('pt_BR')
-  data = {
-
+	Returns:
+		dict: Um dicionário contendo os dados da persona.
+	"""
+	fake = Faker('pt_BR')
+	persona: dict = {
     "username": fake.user_name(),
     "email": fake.email(),
     "password": fake.password(),
@@ -15,17 +20,38 @@ def create_user() -> dict:
     "address":fake.address(),
     "cpf":fake.cpf()
   }
-  print(data["password"])
-  return data
- 
+	return persona
 
-def register_user(data): 
-   response = requests.post(ENDPOINT , json=data)
-   print (response.json())
+def register_user_by_api(persona: dict) -> dict:
+	"""
+    Registra a persona gerada na API.
 
+    Args:
+			persona (dict): Um dicionário contendo os dados da persona a ser registrada.
 
-def main():
-   data = create_user() 
-   register_user(data)
+    Returns:
+			dict: A resposta JSON da API se o registro for bem-sucedido.
 
-main()
+    Ends:
+			O programa com uma mensagem de erro se o registro não for bem-sucedido.
+	"""
+	response: requests.Response = requests.post(ENDPOINT , json=persona)
+	if response.status_code == 201:
+		print("Usuário criado com sucesso!")
+		return(response.json())
+	else:
+		sys.exit("ERR: Houve um problema ao registrar o usuário")
+
+def create_and_regiter_user() -> dict:
+	"""
+	Gera uma persona e a registra na API.
+
+	Retorna:
+			dict: Os dados da persona gerada.
+	"""
+	data = generate_persona() 
+	register_user_by_api(data)
+	return data
+
+if __name__ == "__main__":
+  print(create_and_regiter_user())
